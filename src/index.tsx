f526cbe15b4df1ac8c8e7c8c616636b96ea6e7e8
hpc <philipchanhk626@gmail.com>
2020-07-21 11:38:24 -0400

    hooking up redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 import App from './App'
+import { reducers } from './reducers'
+
+const store = createStore(
+  reducers, 
+  applyMiddleware(thunk)
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App color="red" />
+    <Provider store={store}>
+      <App color="red" />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
